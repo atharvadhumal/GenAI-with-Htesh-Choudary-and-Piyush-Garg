e8717a2b46c3704a415f7f68ddd8e78412a4f672
atharvadhumal <atharvaadhumal@gmail.com>
2026-01-02 15:51:14 +0530

    learning about params

---
+++ 05_functions/09-input-params.py
@@ -0,0 +1,14 @@
+# chai = "Ginger chai"
+
+# def prepare_chai(order):
+#     print("Preparing", order)
+    
+# prepare_chai(chai)
+# print(chai)
+
+chai = [1, 2, 3]
+def edit_chai(cup):
+    cup[1] = 42
+    
+edit_chai(chai)
+print(chai)
